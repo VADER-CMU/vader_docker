b7d83c56b1e65ed003424506b0719945666d5244
KshitijBhat <kshitijmbhat@gmail.com>
2025-10-12 20:03:37 -0400

    Enable ros-noetic container with CUDA support

--- run_docker.sh
+++ run_docker.sh
@@ -1,15 +1,41 @@
+#!/bin/bash
+
+# Allow the container to connect to the host's X server for GUI applications
 xhost +local:root
-docker run --privileged --rm -it \
+
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "x86_64" ]]; then
+  echo "Detected x86_64 architecture. Running vader_img for x86..."
+  docker run --privileged --rm -it \
     --name="vader_docker" \
-    --env="DISPLAY=$DISPLAY" --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
-    --env="NVIDIA_VISIBLE_DEVICES=all" \
-    --volume="$XAUTH:$XAUTH" \
     --network host \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v "$(pwd)/docker_ws:/home/docker_ws" \
-    -v "/etc/localtime:/etc/localtime:ro" \
-    -v "/dev:/dev" -v "/etc/timezone:/etc/timezone:ro" \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /dev:/dev \
+    -w /home/docker_ws \
     --gpus all \
+    vader_img bash
+
+elif [[ "$ARCH" == "aarch64" ]]; then
+  echo "Detected aarch64 (ARM64) architecture. Running vader_img for Orin..."
+  docker run --privileged --rm -it \
+    --name="vader_docker" \
+    --runtime nvidia \
+    --network host \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v "$(pwd)/docker_ws:/home/docker_ws" \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /dev:/dev \
     -w /home/docker_ws \
     vader_img bash
+
+else
+  echo "Error: Unsupported architecture '$ARCH'."
+  exit 1
+fi
--- terminal_docker.sh
+++ terminal_docker.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 xhost +local:docker
 docker exec -it -w /home/docker_ws vader_docker bash 
